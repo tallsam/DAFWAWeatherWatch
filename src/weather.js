d93a9d11fb7f2a2d57f2f7b0e8d4918f65fb2ead
samhassell <sam@webzen.com.au>
2015-07-16 18:08:51 +0800

    forgot to save the latest js

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,5 @@
 /**
- * Load weather data from openweathermap.org and send it to the pebble watchface.
+ * Load weather data from dafwa weather stations and send it to the pebble watchface.
  */
 
 var xhrRequest = function(url, type, callback) {
@@ -13,44 +13,57 @@ var xhrRequest = function(url, type, callback) {
 
 function locationSuccess(pos) {
   var api_key = 'F609B62D128B08EF40486B3E.apikey';
-  var url = 'https://test-api.agric.wa.gov.au/v1/stations'
   
-  
-  var url = 'https://test-api.agric.wa.gov.au/v1/stations.json/nearby/' + pos.coords.latitude + '/' + pos.coords.longitude + '/100?api_key==' + api_key;
-
-  xhrRequest(url, 'GET', function(responseText) {
+  // Get the nearest weather station.
+  var nearbyUrl = 'https://test-api.agric.wa.gov.au/v1/stations.json/nearby/' + pos.coords.latitude + '/' + pos.coords.longitude + '/100?api_key=' + api_key;
+  xhrRequest(nearbyUrl, 'GET', function(responseText) {
     var json = JSON.parse(responseText);
     if (json.cod !== 200) {
       console.log(json.message);
     }
     else {
-      console.log(JSON.stringify(responseText));
-      var airtemp = json.air_temp[0].value;
-      var feelslike = json.feels_like[0].value;
-      var humidity = json.humidity[0].value;
-      var winddirection = json.wind_10min_avg[0].value;
-      var windspeed = json.wind_10min_avg[0].speed_average;
-      var dewpoint = json.dewpoint[0].value;
-      var soiltemp = json.soiltemp[0].soiltemp_degc_ave;
-      var rainfall9am = json.rainfall_from_9am.value;
+      var r = json.results[0];
+      var stationName = r.station_name;
+      var stationCode = r.station_code;
       
-      var dictionary = {
-        'KEY_AIRTEMP': airtemp,
-        'KEY_FEELSLIKE': feelslike,
-        'KEY_HUMIDITY': humidity,
-        'KEY_WINDDIRECTION': winddirection,
-        'KEY_WINDSPEED': windspeed,
-        'KEY_DEWPOINT': dewpoint,
-        'KEY_SOILTEMP': soiltemp,
-        'KEY_RAINFALL9AM': rainfall9am
-      };
-      
-      Pebble.sendAppMessage(dictionary, function(e) {
-        console.log('Weather info sent to Pebble successfully!');
-      }, function(e) {
-        console.log('Error sending weather info to Pebble!');
-      }
-     ); 
+      // Now load the weather data
+      var weatherUrl = 'https://test-api.agric.wa.gov.au/v1/stations.json/'+ stationCode +'/latest?api_key=' + api_key;
+      xhrRequest(weatherUrl, 'GET', function(responseText) {
+        var json = JSON.parse(responseText);
+        if (json.cod !== 200) {
+          console.log(json.message);
+        }
+        else {
+          console.log(JSON.stringify(responseText));
+          var airTemp = json.air_temp[0].value;
+          var feelsLike = json.feels_like[0].value;
+          var humidity = json.humidity[0].value;
+          var windDirection = json.wind_10min_avg[0].value;
+          var windSpeed = json.wind_10min_avg[0].speed_average;
+          var dewPoint = json.dewpoint[0].value;
+          var soilTemp = json.soiltemp[0].soiltemp_degc_ave;
+          var rainfall9am = json.rainfall_from_9am.value;
+          
+          var dictionary = {
+            'KEY_STATIONNAME': stationName,
+            'KEY_STATIONCODE': stationCode,
+            'KEY_AIRTEMP': airTemp,
+            'KEY_FEELSLIKE': feelsLike,
+            'KEY_HUMIDITY': humidity,
+            'KEY_WINDDIRECTION': windDirection,
+            'KEY_WINDSPEED': windSpeed,
+            'KEY_DEWPOINT': dewPoint,
+            'KEY_SOILTEMP': soilTemp,
+            'KEY_RAINFALL9AM': rainfall9am
+          };
+        
+          Pebble.sendAppMessage(dictionary, function(e) {
+            console.log('Weather info sent to Pebble successfully!');
+          }, function(e) {
+            console.log('Error sending weather info to Pebble!');
+          }); 
+        }
+      });
     }
   });
 }
